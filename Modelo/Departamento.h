b2e4a70cf5f3400d9be7151134b9b3ab80a0596f
rafael-rosseto <rafaelbassirosseto@gmail.com>
2020-09-04 20:43:52 -0300

    update

--- Modelo/Departamento.h
+++ Modelo/Departamento.h
@@ -5,64 +5,72 @@
 #include "./Funcionario.h"
 #define MAX 50
 
-class Departamento{
-    
-    private:
-        string codigo, nome;
-        static int TotalFuncionarios;
-        int NFuncionarios;
-        Funcionario* funcionarios[MAX];
-        
-    public:
-        Departamento(){}
-        Departamento(string _nome, string _codigo){
-            codigo=_codigo;
-            nome=_nome;
-            NFuncionarios=0;
-            for(int i=0; i<MAX; i++) funcionarios[i] = nullptr;
-        } 
-       
-        void AddFuncionario(Funcionario* funcionario){
-            funcionarios[NFuncionarios]=funcionario;
-            NFuncionarios++;
-        }
+class Departamento
+{
 
-        double calculaGastos(){
-            double gastos=0;
-            for(int i=0; i<NFuncionarios; i++) gastos+=funcionarios[i]->calculaSalario();
-            return gastos;
-        }
-        
-        Funcionario** getFuncionarios(){
-            return funcionarios;
-        }
-        
-        int getNFuncionarios(){
-            return NFuncionarios;
-        }
+private:
+    string codigo, nome;
+    static int TotalFuncionarios;
+    int NFuncionarios;
+    Funcionario *funcionarios[MAX];
 
-        string getCodigo(){
-            return codigo;
-        }
+public:
+    Departamento() {}
+    Departamento(string _nome, string _codigo)
+    {
+        codigo = _codigo;
+        nome = _nome;
+        NFuncionarios = 0;
+        for (int i = 0; i < MAX; i++)
+            funcionarios[i] = nullptr;
+    }
 
-        string getNome(){
-            return nome;
-        }
+    void AddFuncionario(Funcionario *funcionario)
+    {
+        funcionarios[NFuncionarios] = funcionario;
+        NFuncionarios++;
+    }
 
-        void setCodigo(string _codigo){
-            codigo=_codigo;
-        }
+    double calculaGastos()
+    {
+        double gastos = 0;
+        for (int i = 0; i < NFuncionarios; i++)
+            gastos += funcionarios[i]->calculaSalario();
+        return gastos;
+    }
 
-        void setNome(string _nome){
-            nome=_nome;
-        }
+    Funcionario **getFuncionarios()
+    {
+        return funcionarios;
+    }
 
-        void Informacoes(){
-            cout<<
-            "\n\nNome:"<< nome<<
-            "\nCódigo:"<<codigo<<
-            "\nNúmero de funcionarios:"<<NFuncionarios<<
-            "\nGasto total:"<< calculaGastos();
-        }
+    int getNFuncionarios()
+    {
+        return NFuncionarios;
+    }
+
+    string getCodigo()
+    {
+        return codigo;
+    }
+
+    string getNome()
+    {
+        return nome;
+    }
+
+    void setCodigo(string _codigo)
+    {
+        codigo = _codigo;
+    }
+
+    void setNome(string _nome)
+    {
+        nome = _nome;
+    }
+
+    void Informacoes()
+    {
+        cout << "\n\nNome:" << nome << "\nCódigo:" << codigo << "\nNúmero de funcionarios:" << NFuncionarios << "\nGasto total:" << calculaGastos();
+    }
 };
-
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Trabalho Programação Orientada a Objetos 1
 
-Rafael Rosseto
 Vinicius Polachini
+Rafael Rosseto
 
 Trabalho implementado em c++ utilizando as ferramentas disponibilizadas pelo QT Open Source disponível em https://www.qt.io/
